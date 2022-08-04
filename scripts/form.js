aa4b29fa917cd95e933bbe4bb8bb2cf3d0ba92fd
Mayur Wankhade <mayur.wankhade2@gmail.com>
2022-08-04 08:19:34 +0530

    Add employee data to local storage

--- scripts/form.js
+++
@@ -1,7 +0,0 @@
-const salary = document.querySelector("#salary");
-const salaryOutput = document.querySelector(".salary-output");
-
-salaryOutput.textContent = salary.value;
-salary.addEventListener("input", function () {
-  salaryOutput.textContent = salary.value;
-});
